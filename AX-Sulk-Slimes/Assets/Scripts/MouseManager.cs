a78c3b7561106433d2fa0597d51d2dd705fe6457
Markham <markhamonca@codeninjas.com>
2024-08-25 13:46:52 -0400

    31

--- AX-Sulk-Slimes/Assets/Scripts/MouseManager.cs
+++ AX-Sulk-Slimes/Assets/Scripts/MouseManager.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class MouseManager : MonoBehaviour
 {
+    [Header("Mouse Info")]
+    public Vector3 clickStartLocation;
+    
+    [Header("Physics")]
+    public Vector3 launchVector;
+    public float launchForce;
+
+    [Header("Slime")]
+    public Transform slimeTransform;
+    public Rigidbody slimeRigidbody;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,25 @@ public class MouseManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickStartLocation = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mouseDifference = clickStartLocation - Input.mousePosition;
+            launchVector = new Vector3(
+                mouseDifference.x * 1f,
+                mouseDifference.y * 1.2f,
+                mouseDifference.y * 1.5f
+                );
+            launchVector.Normalize();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            slimeRigidbody.isKinematic = false;
+            slimeRigidbody.AddForce(launchVector * launchForce, ForceMode.Force);
+            print("Release!");
+        }
     }
 }
